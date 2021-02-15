25a917b23229542556ed381d4075283c1a8cb004
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-02-15 15:28:16 -0800

    Commit

---
+++ Subscription Box/AlienCellTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  AlienCellTableViewCell.swift
+//  Subscription Box
+//
+//  Created by Ryan Cornel on 2/15/21.
+//
+
+import UIKit
+
+class AlienCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    //initializer for a cell
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+
+    func setup() {
+
+    }
+
+}
