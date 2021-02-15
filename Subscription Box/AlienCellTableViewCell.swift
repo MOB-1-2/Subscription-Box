e91d2ff2d673d67d053bd28bce8710266e814dd5
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-02-15 15:29:14 -0800

    Commit

--- Subscription Box/AlienCellTableViewCell.swift
+++ Subscription Box/AlienCellTableViewCell.swift
@@ -32,7 +32,22 @@ class AlienCellTableViewCell: UITableViewCell {
     }
 
     func setup() {
+        
+        self.contentView.addSubview(background)
+        background.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 10).isActive = true
+        background.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
+        background.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        background.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
 
     }
+    
+    let background : UIView = {
+      let background = UIView()
+      background.backgroundColor = #colorLiteral(red: 0.8186396956, green: 0.7955000997, blue: 1, alpha: 1)
+      background.layer.cornerRadius = 5
+      background.layer.masksToBounds = true
+      background.translatesAutoresizingMaskIntoConstraints = false
+      return background
+    }()
 
 }
