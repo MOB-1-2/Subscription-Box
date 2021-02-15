5c0a952597c740f2053f49770c63d8dd4973a675
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-02-15 07:41:02 -0800

    Commit

--- Subscription Box/ViewController.swift
+++ Subscription Box/ViewController.swift
@@ -10,12 +10,44 @@ import UIKit
 class ViewController: UIViewController {
     
     var scrollView: UIScrollView!
+    var container: UIStackView!
+    var firstView: UIView!
+    var secondView: UIView!
+    var thirdView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        container = UIStackView()
+        container.axis = .horizontal
+        container.spacing = 0
+        container.distribution = .fillEqually
+        container.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(container)
+        
+        container.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        container.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        container.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        container.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        container.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+        
+        
+        
+        
+        
+        
     }
 
+    
 
 }
 
